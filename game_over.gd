8e55c6e0d9bf817142aa690c5ae2a50272d2d3c1
aldrinn <48447138+aldringordon@users.noreply.github.com>
2025-08-16 23:42:33 +0800

    add reset game button in game over screen

--- game_over.gd
+++ game_over.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 signal open_menu
+signal restart_game
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,9 +16,15 @@ func _process(delta: float) -> void:
 func show_game_over_screen(message: String, score: Array):
 	$WinnerMessage.text = message
 	$ScoreMessage.text = str(score[0]) + " - " + str(score[1])
+	
 	show()
 
 
 func _on_menu_button_pressed() -> void:
 	$ClickSound.play()
 	open_menu.emit()
+
+
+func _on_restart_button_pressed() -> void:
+	$ClickSound.play()
+	restart_game.emit()
--- main.gd
+++ main.gd
@@ -2,7 +2,9 @@ extends Sprite2D
 
 var score := [0, 0] # [0: Player, 1: CPU]
 const PADDLE_SPEED : int = 500
+
 var MAX_SCORE : int = 1
+var DIFFICULTY: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,35 +21,38 @@ func show_main_menu():
 	$PixelFantasiaSound.play()
 
 func start_new_game(difficulty: String, maxScore: int):
-	set_paddle_speed(difficulty)
+	DIFFICULTY = difficulty
 	MAX_SCORE = maxScore
+	set_paddle_speed(DIFFICULTY)
 	score = [0, 0]
 	$Hud/CPUScore.text = "0"
 	$Hud/PlayerScore.text = "0"
 	$Player.position = Vector2(50, 324)
 	$CPU.position = Vector2(1082, 324)
 	$Menu.hide()
+	$GameOver.hide()
 	$Hud.show()
 	$BallTimer.start()
 	$PixelFantasiaSound.stop()
 	$AmbientSound.play()
 
+func reset_game():
+	start_new_game(DIFFICULTY, MAX_SCORE)
+
 func set_paddle_speed(difficulty: String):
 	if difficulty == "Easy":
 		$Ball.START_SPEED = 350
 		$CPU.p_speed = 200
-		MAX_SCORE = 3
 	if difficulty == "Normal":
 		$Ball.START_SPEED = 500
 		$CPU.p_speed = 500
-		MAX_SCORE = 5
 	if difficulty == "Hard":
 		$Ball.START_SPEED = 800
 		$CPU.p_speed = 800
-		MAX_SCORE = 8
 
 func end_game():
 	$AmbientSound.stop()
+	await get_tree().create_timer(1.0).timeout
 	if score[0] == MAX_SCORE:
 		$GameOver.show_game_over_screen("You Won", score)
 		$VictorySound.play()
